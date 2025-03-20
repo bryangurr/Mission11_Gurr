d1eff5be9d4b4a0a91756e063dbd1664e7a24fb1
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-03-19 21:24:14 -0600

    Add pagination and sorting (ascending)

--- backend/Amazon.API/obj/Debug/net8.0/Amazon.API.AssemblyInfo.cs
+++ backend/Amazon.API/obj/Debug/net8.0/Amazon.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Amazon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bfc03f5b0535fe4614f8bf3d604750a906fbdff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996c4c8000ba563d60c7654cc701db5bbebe7cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Amazon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Amazon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/BookList.tsx
+++ frontend/src/BookList.tsx
@@ -3,19 +3,45 @@ import { Book } from './types/Book';
 
 function BookList() {
   const [Books, setBooks] = useState<Book[]>([]);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [orderBy, setOrder] = useState<string>('BookID');
 
   useEffect(() => {
     const fetchBooks = async () => {
-      const response = await fetch('https://localhost:5000/api/Book/AllBooks');
+      const response = await fetch(
+        `https://localhost:5000/api/Book/AllBooks?numRecords=${pageSize}&orderBy=${orderBy}&pageNum=${pageNum}`
+      );
       const data = await response.json();
-      setBooks(data);
+      setBooks(data.books);
+      setTotalItems(data.totalBooks);
+      setTotalPages(Math.ceil(totalItems / pageSize));
     };
     fetchBooks();
-  }, []);
+  }, [pageSize, pageNum, totalItems, orderBy]);
 
   return (
     <>
       <h1>Books</h1>
+      <br />
+      <label>
+        Order by:&emsp;
+        <select
+          onChange={(o) => {
+            setOrder(o.target.value);
+            setPageNum(1);
+          }}
+        >
+          <option value="BookID">---Select---</option>
+          <option value="Title">Title</option>
+          <option value="Publisher">Publisher</option>
+          <option value="Category">Category</option>
+          <option value="Classification">Classification</option>
+        </select>
+      </label>
+      <br />
       <br />
       {Books.map((b) => (
         <div id="bookCard" className="card" key={b.bookID}>
@@ -34,9 +60,38 @@ function BookList() {
         </div>
       ))}
       <br />
+
+      <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>
+        Previous
+      </button>
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index + 1}
+          onClick={() => setPageNum(index + 1)}
+          disabled={pageNum === index + 1}
+        >
+          {index + 1}
+        </button>
+      ))}
+      <button
+        disabled={pageNum === totalPages}
+        onClick={() => setPageNum(pageNum + 1)}
+      >
+        Next
+      </button>
+      <br />
       <label>
-        Results per page:
-        <select>
+        Results per page:&emsp;
+        <select
+          value={pageSize}
+          onChange={(p) => {
+            var oldSize = pageSize;
+            const newSize = Number(p.target.value);
+
+            setPageSize(Number(p.target.value));
+            setPageNum(Math.ceil(((pageNum - 1) * oldSize + 1) / newSize));
+          }}
+        >
           <option value="5">5</option>
           <option value="10">10</option>
           <option value="20">20</option>
