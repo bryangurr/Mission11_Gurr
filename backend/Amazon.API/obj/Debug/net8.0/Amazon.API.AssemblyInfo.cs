21adbf58a36f41f74a168f5bb6c9626fd8303ce4
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-03-20 16:35:33 -0600

    More cookies stuff

--- backend/Amazon.API/obj/Debug/net8.0/Amazon.API.AssemblyInfo.cs
+++ backend/Amazon.API/obj/Debug/net8.0/Amazon.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Amazon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1eff5be9d4b4a0a91756e063dbd1664e7a24fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a4eca88d11f8404f9823525896153d6668a886")]
 [assembly: System.Reflection.AssemblyProductAttribute("Amazon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Amazon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
