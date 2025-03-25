8b3e88cbffb755ffdb39342b2b924f4298d7258f
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-03-24 21:06:04 -0600

    Create AllBoksPage and clean up App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import BookList from './BookList';
-import CategoryFilter from './CategoryFilter';
-import WelcomeHeader from './WelcomeHeader';
+import AllBooksPage from './pages/AllBooksPage';
 
 function App() {
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <WelcomeHeader />
-        </div>
-        <div className="row">
-          <div className="col-md-3">
-            <CategoryFilter
-              selectedCategories={selectedCategories}
-              setSelectedCategories={setSelectedCategories}
-            />
-          </div>
-          <div className="col-md-9">
-            <BookList selectedCategories={selectedCategories} />
-          </div>
-        </div>
-      </div>
+      <AllBooksPage />
     </>
   );
 }
---
+++ frontend/src/pages/AllBooksPage.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import BookList from '../BookList';
+import CategoryFilter from '../CategoryFilter';
+import WelcomeHeader from '../WelcomeHeader';
+
+function AllBooksPage() {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <WelcomeHeader />
+      </div>
+      <div className="row">
+        <div className="col-md-3">
+          <CategoryFilter
+            selectedCategories={selectedCategories}
+            setSelectedCategories={setSelectedCategories}
+          />
+        </div>
+        <div className="col-md-9">
+          <BookList selectedCategories={selectedCategories} />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AllBooksPage;
